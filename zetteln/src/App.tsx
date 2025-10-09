16fe6110230633fea50a19ec318ce0f456b7b0e1
lnwlk <hello@lenawilke.de>
2025-10-09 17:58:16 +0200

    Twipla

--- zetteln/src/App.tsx
+++ zetteln/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Nav from './components/Nav'
+import { useTwiplaAnalytics } from './hooks/useTwiplaAnalytics'
 import AppWaitlistPage from './pages/AppWaitlistPage'
 import HomePage from './pages/HomePage'
 import ImprintPage from './pages/ImprintPage'
 
 export default function App() {
+  useTwiplaAnalytics()
   return (
     <div className="bg-sand-50 text-black-800 relative text-lg font-light">
       <Nav />
